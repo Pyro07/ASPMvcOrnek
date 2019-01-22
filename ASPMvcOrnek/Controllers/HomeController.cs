39bbe866643221cc71225d8002edb96b8b97a7fa
Pyro07 <burakbaseskioglu@gmail.com>
2019-01-22 23:00:48 +0300

    ASP.Net MVC
    MVC metodolojisini kullanarak girilen verileri Cache bellekte tutarak basit bir bilgisayar sistem toplama örneği

---
+++ ASPMvcOrnek/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPMvcOrnek.Models;
+
+namespace ASPMvcOrnek.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListeKaydet(string Islemci, string Anakart, string Ram, string SolidDrive, string HardDrive, string PowerSupply)
+        {
+            if(HttpRuntime.Cache["Bilesenler"] == null)
+            {
+                List<Liste> BilesenListesi = new List<Liste>();
+                Liste listeler = new Liste();
+                listeler.Islemci = Islemci;
+                listeler.Anakart = Anakart;
+                listeler.Ram = Ram;
+                listeler.SolidDrive = SolidDrive;
+                listeler.HardDrive = HardDrive;
+                listeler.PowerSupply = PowerSupply;
+
+                BilesenListesi.Add(listeler);
+                HttpRuntime.Cache["Bilesenler"] = BilesenListesi;
+            }
+            else
+            {
+                List<Liste> BilesenListesi = (List<Liste>)HttpRuntime.Cache["Bilesenler"];
+                Liste listeler = new Liste();
+                listeler.Islemci = Islemci;
+                listeler.Anakart = Anakart;
+                listeler.Ram = Ram;
+                listeler.SolidDrive = SolidDrive;
+                listeler.HardDrive = HardDrive;
+                listeler.PowerSupply = PowerSupply;
+
+                BilesenListesi.Add(listeler);
+                HttpRuntime.Cache["Bilesenler"] = BilesenListesi;
+            }
+            return RedirectToAction("BilesenListesi");
+        }
+
+        public ActionResult BilesenListesi()
+        {
+            var model = (List<Liste>)HttpRuntime.Cache["Bilesenler"];
+            return View(model);
+        }
+    }
+}
---
+++ ASPMvcOrnek/Models/Liste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPMvcOrnek.Models
+{
+    public class Liste
+    {
+        public string Islemci { get; set; }
+        public string Anakart { get; set; }
+        public string Ram { get; set; }
+        public string SolidDrive { get; set; }
+        public string HardDrive { get; set; }
+        public string PowerSupply { get; set; }
+    }
+}
